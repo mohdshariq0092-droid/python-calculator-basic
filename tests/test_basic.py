c01377467772cbfff289f9f2bdc1e553625160fe
Mohd Shariq <mohd.shariq@ignitarium.com>
2026-01-13 09:24:59 +0530

    Update code for basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,12 @@
+import pytest
 from calculator.basic import add, subtract
 
 
+@pytest.mark.sanity
 def test_add():
     assert add(2, 3) == 5
 
 
+@pytest.mark.sanity
 def test_subtract():
     assert subtract(5, 3) == 2
